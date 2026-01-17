f054266a6f0d9a3ab214a261e7e25eb903396b5e
cringlekaden <cringlekaden@gmail.com>
2026-01-17 13:22:11 -0500

    Initial Commit

---
+++ MetalCup/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetalCup
+//
+//  Created by Kaden Cringle on 1/17/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
